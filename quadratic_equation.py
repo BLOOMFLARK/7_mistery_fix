922246f21f83f94b657906aeb423624915a31ad7
BLOOMFLARK <50524041+BLOOMFLARK@users.noreply.github.com>
2019-05-13 20:28:24 +0300

    a!=0

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -2,22 +2,14 @@ from math import sqrt
 
 
 def get_roots(a, b, c):
-    if a == 0:
-
-        if b == 0:
-            return None
-        else:
-            return -b/c, None
-
+    discriminant = b ** 2 - 4 * a * c
+    if discriminant < 0:
+        return None, None
     else:
-        discriminant = b ** 2 - 4 * a * c
-        if discriminant < 0:
-            return None, None
-        else:
-            root1 = (-b - sqrt(discriminant)) / (2 * a)
-            root2 = (-b + sqrt(discriminant)) / (2 * a)
-            if discriminant == 0:
-                return root1, None
-            elif discriminant > 0:
-                return root1, root2
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
+        root2 = (-b + sqrt(discriminant)) / (2 * a)
+        if discriminant == 0:
+            return root1, None
+        elif discriminant > 0:
+            return root1, root2
 
